ddf705424ce5acc46074d6e223a046f417981d3f
Faith Makinde <faithmakinde0@gmail.com>
2022-08-26 22:53:18 +0100

    added a delete button

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const saveBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-let inputEl = document.getElementById("input-el");
-let saveBtn = document.getElementById("input-btn");
-let ulEl = document.getElementById("ul-el");
-saveBtn.addEventListener("click", function(){
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  renderLeads();
-})
+if(leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
 
-function renderLeads(){
-    let listItems = "";
-  for(let i = 0; i < myLeads.length; i++){
-    listItems += `
-    <li>
-      <a href='${myLeads}'>
-      ${myLeads[i]}
-      </a>
-    </li>`
+
+function render(leads){
+  let listItems = "";
+for(let i = 0; i < leads.length; i++){
+  listItems += `
+  <li>
+    <a href='${leads[i]}'>
+    ${leads[i]}
+    </a>
+  </li>`
 }
 ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+})
+saveBtn.addEventListener("click", function(){
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads);
+})
