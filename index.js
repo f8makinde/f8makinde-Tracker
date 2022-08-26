0dc542effe4d436b6f5e6ab4a03042f03ac0d112
Faith Makinde <faithmakinde0@gmail.com>
2022-08-27 00:25:18 +0100

    work-completed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const saveBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn")
 
 if(leadsFromLocalStorage){
   myLeads = leadsFromLocalStorage;
@@ -16,13 +17,24 @@ function render(leads){
 for(let i = 0; i < leads.length; i++){
   listItems += `
   <li>
-    <a href='${leads[i]}'>
+    <a href='${leads[i]}' target='blank'>
     ${leads[i]}
     </a>
   </li>`
 }
 ulEl.innerHTML = listItems;
 }
+
+
+
+tabBtn.addEventListener("click", function(){
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+})
+
+})
 
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear();
